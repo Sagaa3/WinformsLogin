e8682de7512d5c17532ed9b2a09588a24777b4e3
Sagaa <anafi123@Mail.ru>
2025-01-23 17:03:31 +0300

    2

---
+++ WinformsLogin/Model/Password.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinformsLogin.Model
+{
+    public class Password
+    {
+        public int PasswordId {  get; set; }
+        public string Pass { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ WinformsLogin/contex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinformsLogin.Model;
+
+namespace WinformsLogin
+{
+    internal class contex : DbContext
+    {
+        string connection = "host=localhost; port=5432; database=EfcoreTestDb; user id=postgres; password=root";
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Password> Passwords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connection);
+        }
+    }
+}
